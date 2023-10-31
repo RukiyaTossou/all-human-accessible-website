bd0dba24a5230a0b55d0de016f6d31133a9a302c
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2023-10-31 11:16:12 +0100

    home pagina

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+const body = document.querySelector('body'),
+sidebar = body.querySelector('nav'),
+toggle = body.querySelector(".toggle"),
+searchBtn = body.querySelector(".search-box"),
+modeSwitch = body.querySelector(".toggle-switch"),
+modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
+
+// toggle button voor uitleengeschiednis 
+
+function toggleList(toggleIconId, listId) {
+
+    
+    const list = document.getElementById(listId);// parameters geven voor meer dan 1x gebruik
+    const icon = document.getElementById(toggleIconId);
+
+    if (list.style.display === "none" || list.style.display === "") { // checken of de list verbrogen of leeg is.
+        //als de list verborgen, kaat het zien (block)
+        list.style.display = "block";
+        icon.innerText = "-"; // kan op worden geklikt om het te verbergen 
+    } else {
+        //als de list te zien is verberg de list.
+        list.style.display = "none";
+        icon.innerText = "+"; // kan op worden geklik om het weer te laten zien 
+    }
+}
+
+// Hamburger menu
+const toggleButton = document.querySelector('.toggle-button');
+const Sidebar = document.querySelector('.sidebar');
+
+toggleButton.addEventListener('click', () => {
+    Sidebar.classList.toggle('active');
+});
