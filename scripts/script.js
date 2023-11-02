9c6501b7be0cfd896f05dc902989d2067a75552e
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2023-11-02 20:35:15 +0100

    pijltjes doen het nu weer!

--- scripts/script.js
+++ scripts/script.js
@@ -51,3 +51,77 @@ const Sidebar = document.querySelector('.sidebar');
 toggleButton.addEventListener('click', () => {
     Sidebar.classList.toggle('active');
 });
+
+// Scroll knoppen
+document.addEventListener("DOMContentLoaded", function () {
+    const ulBlok = document.querySelector(".ul-blok");
+    const scrollLeftButton = document.querySelector(".scroll-links");
+    const scrollRightButton = document.querySelector(".scroll-rechts");
+
+    let scrollAmount = 100; // het aantal pixels om te scrollen
+
+    scrollLeftButton.addEventListener("click", function () {
+        ulBlok.scrollLeft -= scrollAmount;
+    });
+
+    scrollRightButton.addEventListener("click", function () {
+        ulBlok.scrollLeft += scrollAmount;
+    });
+});
+
+
+//     // For the second set of buttons
+//     const ulBlok2 = document.querySelector(".ul-blok2"); // Adjust the selector
+//     const scrollLeftButton2 = document.querySelector(".scroll-links2"); // Adjust the selector
+//     const scrollRightButton2 = document.querySelector(".scroll-rechts2"); // Adjust the selector
+
+//     scrollLeftButton2.addEventListener("click", function () {
+//         ulBlok2.scrollLeft -= scrollAmount;
+//     });
+
+//     scrollRightButton2.addEventListener("click", function () {
+//         ulBlok2.scrollLeft += scrollAmount;
+//     });
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Plaats hier de JavaScript-code
+//     const scrollButtonContainers = document.querySelectorAll(".scroll-buttons");
+
+//     scrollButtonContainers.forEach(function (container) {
+//         const ulBlok = container.querySelector(".ul-blok");
+//         const scrollLeftButton = container.querySelector(".scroll-links");
+//         const scrollRightButton = container.querySelector(".scroll-rechts");
+
+//         let scrollAmount = 300;
+
+//         scrollLeftButton.addEventListener("click", function () {
+//             ulBlok.scrollLeft -= scrollAmount;
+//         });
+
+//         scrollRightButton.addEventListener("click", function () {
+//             ulBlok.scrollLeft += scrollAmount;
+//         });
+//     });
+// });
+
+
+
+// boeken knop boek blok 
+document.addEventListener("DOMContentLoaded", function () {
+    const boekenButton = document.getElementById("boeken-button");
+    const boekDetails = document.querySelector(".boek-details .ul-blok");
+
+    boekenButton.addEventListener("click", function () {
+        // Controleer of de elementen momenteel verborgen zijn
+        if (boekDetails.style.display === "none") {
+            // Toon de elementen
+            boekDetails.style.display = "block";
+        } else {
+            // Verberg de elementen
+            boekDetails.style.display = "none";
+        }
+    });
+});
+
